23e63c07916243fc9411e06aab9d198f414497d8
ArthurDorokhin <a.u.dorokhin@gmail.com>
2019-12-05 17:17:41 +0200

    add new search

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -9,21 +9,105 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data : [
-                {img: '../img/owl.svg', nameListUser: 'Chad Snyder', ageListUser: '28', phoneListUser: '8 (629) 653-9041', id: '1'},
-                {img: '../img/cat.svg', nameListUser: 'Milton Warner', ageListUser: '11', phoneListUser: '8 (435) 653-9041', id: '2'},
-                {img: '../img/fox.svg', nameListUser: 'Viola Hale', ageListUser: '22', phoneListUser: '8 (543) 653-9041', id: '3'},
-                {img: '../img/koala.svg', nameListUser: 'Tyler Herrera', ageListUser: '34', phoneListUser: '8 (656) 653-9041', id: '4'},
-                {img: '../img/lion.svg', nameListUser: 'Gabriel Howell', ageListUser: '12', phoneListUser: '8 (123) 653-9041', id: '5'},
-                {img: '../img/penguin.svg', nameListUser: 'Adelaide Jacobs', ageListUser: '34', phoneListUser: '8 (543) 653-9041', id: '6'},
-                {img: '../img/pig.svg', nameListUser: 'James Diaz', ageListUser: '76', phoneListUser: '8 (875) 653-9041', id: '7'},
-                {img: '../img/raccoon.svg', nameListUser: 'Vincent Sparks', ageListUser: '54', phoneListUser: '8 (986) 653-9041', id: '8'},
-                {img: '../img/sheep.svg', nameListUser: 'Mark Rhodes', ageListUser: '12', phoneListUser: '8 (467) 653-9041', id: '9'},
-                {img: '../img/fox.svg', nameListUser: 'Gordon Daniels', ageListUser: '32', phoneListUser: '8 (978) 653-9041', id: '10'},
-                {img: '../img/koala.svg', nameListUser: 'Ella Parks', ageListUser: '43', phoneListUser: '8 (232) 653-9041', id: '11'},
-                {img: '../img/lion.svg', nameListUser: 'Luis Jacobs', ageListUser: '45', phoneListUser: '8 (547) 653-9041', id: '12'},
-                {img: '../img/penguin.svg', nameListUser: 'Phillip Daniels', ageListUser: '65', phoneListUser: '8 (629) 653-9041', id: '13'},
-                {img: '../img/cat.svg', nameListUser: 'Hettie Soto', ageListUser: '67', phoneListUser: '8 (123) 653-9041', id: '14'}
+            data: [
+                {
+                    img: '../img/owl.svg',
+                    nameListUser: 'Chad Snyder',
+                    ageListUser: '28',
+                    phoneListUser: '8 (629) 653-9041',
+                    id: '1'
+                },
+                {
+                    img: '../img/cat.svg',
+                    nameListUser: 'Milton Warner',
+                    ageListUser: '11',
+                    phoneListUser: '8 (435) 653-9041',
+                    id: '2'
+                },
+                {
+                    img: '../img/fox.svg',
+                    nameListUser: 'Viola Hale',
+                    ageListUser: '22',
+                    phoneListUser: '8 (543) 653-9041',
+                    id: '3'
+                },
+                {
+                    img: '../img/koala.svg',
+                    nameListUser: 'Tyler Herrera',
+                    ageListUser: '34',
+                    phoneListUser: '8 (656) 653-9041',
+                    id: '4'
+                },
+                {
+                    img: '../img/lion.svg',
+                    nameListUser: 'Gabriel Howell',
+                    ageListUser: '12',
+                    phoneListUser: '8 (123) 653-9041',
+                    id: '5'
+                },
+                {
+                    img: '../img/penguin.svg',
+                    nameListUser: 'Adelaide Jacobs',
+                    ageListUser: '34',
+                    phoneListUser: '8 (543) 653-9041',
+                    id: '6'
+                },
+                {
+                    img: '../img/pig.svg',
+                    nameListUser: 'James Diaz',
+                    ageListUser: '76',
+                    phoneListUser: '8 (875) 653-9041',
+                    id: '7'
+                },
+                {
+                    img: '../img/raccoon.svg',
+                    nameListUser: 'Vincent Sparks',
+                    ageListUser: '54',
+                    phoneListUser: '8 (986) 653-9041',
+                    id: '8'
+                },
+                {
+                    img: '../img/sheep.svg',
+                    nameListUser: 'Mark Rhodes',
+                    ageListUser: '12',
+                    phoneListUser: '8 (467) 653-9041',
+                    id: '9'
+                },
+                {
+                    img: '../img/fox.svg',
+                    nameListUser: 'Gordon Daniels',
+                    ageListUser: '32',
+                    phoneListUser: '8 (978) 653-9041',
+                    id: '10'
+                },
+                {
+                    img: '../img/koala.svg',
+                    nameListUser: 'Ella Parks',
+                    ageListUser: '43',
+                    phoneListUser: '8 (232) 653-9041',
+                    id: '11'
+                },
+                {
+                    img: '../img/lion.svg',
+                    nameListUser: 'Luis Jacobs',
+                    ageListUser: '45',
+                    phoneListUser: '8 (547) 653-9041',
+                    id: '12'
+                },
+                {
+                    img: '../img/penguin.svg',
+                    nameListUser: 'Phillip Daniels',
+                    ageListUser: '65',
+                    phoneListUser: '8 (629) 653-9041',
+                    id: '13'
+                },
+                {
+                    img: '../img/cat.svg',
+                    nameListUser: 'Hettie Soto',
+                    ageListUser: '67',
+                    phoneListUser: '8 (123) 653-9041',
+                    id: '14'
+                }
             ],
             term: ''
         };
@@ -35,11 +119,18 @@ export default class App extends Component {
 
         if (term.length === 0) {
             return items
+        } else {
+            console.log('Nothing found :(');
         }
 
         return items.filter((item) => {
-           return item.nameListUser.indexOf(term) > -1
-        });
+            return (
+                item.nameListUser.indexOf(term) > -1 ||
+                item.ageListUser.indexOf(term) > -1  ||
+                item.phoneListUser.indexOf(term) > -1
+
+            )
+        })
 
     }
 
@@ -59,7 +150,7 @@ export default class App extends Component {
                 <ControlPanel/>
                 <div className="main-content">
                     <SelectUser/>
-                    <UserList lists={visiblePosts} />
+                    <UserList lists={visiblePosts}/>
                 </div>
             </div>
         )
