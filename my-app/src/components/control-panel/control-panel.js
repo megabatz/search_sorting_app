5404f188e103ce54a4f4a3e4bed9bd59af196c23
ArthurDorokhin <a.u.dorokhin@gmail.com>
2019-12-06 17:19:46 +0200

    class control-panel

--- my-app/src/components/control-panel/control-panel.js
+++ my-app/src/components/control-panel/control-panel.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './control-panel.css';
 
-const ControlPanel = () => {
-  return (
-      <div className="groupBtn">
-          <button className="sortByName"><span><img src="../img/sort-name.png" alt=""/></span> Sort by name</button>
-          <button className="sortByAge"><span><img src="../img/sort-age.svg" alt=""/></span> Sort by age</button>
-          <button className="reset"><span><img src="../img/reset.png" alt=""/></span> Reset</button>
-      </div>
-  )
-};
+export default class ControlPanel extends Comment {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-export default ControlPanel;
+        }
+    }
+
+    render() {
+        return (
+            <div className="groupBtn">
+                <button className="sortByName"><span><img src="../img/sort-name.png" alt=""/></span> Sort by name</button>
+                <button className="sortByAge"><span><img src="../img/sort-age.svg" alt=""/></span> Sort by age</button>
+                <button className="reset"><span><img src="../img/reset.png" alt=""/></span> Reset</button>
+            </div>
+        )
+    }
+
+};
